6fe6013352fed2ccd82e686facb9d060e771320f
markes1977 <smarkey@ncontrolsec.com>
2023-03-14 16:13:36 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
+import validators
 
 file1 = open('urls.txt', 'r')
 Lines = file1.readlines()
 
 for line in Lines:
-    response = requests.get(line.strip())
-    print(response.status_code)
+    if validators.url(line.strip()):
+        response = requests.get(line.strip())
+        print(line.strip(), response.status_code)
+    elif validators.url("http://" + line.strip()):
+        response = requests.get("http://" + line.strip())
+        print("http://" + line.strip(), response.status_code)
+    elif validators.url("http://www." + line.strip()):
+        response = requests.get("http://www." + line.strip())
+        print("http://www." + line.strip(), response.status_code)
