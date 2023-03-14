56a3a793d09165ad4830090c13d273741b5ca7f0
markes1977 <smarkey@ncontrolsec.com>
2023-03-14 18:57:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ Lines = file1.readlines()
 for line in Lines:
     if validators.url(line.strip()):
         response = requests.get(line.strip())
-        print(line.strip(), response.status_code)
+        print(line.strip(), " ", response.status_code)
     elif validators.url("http://" + line.strip()):
         response = requests.get("http://" + line.strip())
-        print("http://" + line.strip(), response.status_code)
+        print("http://" + line.strip(), " ", response.status_code)
     elif validators.url("http://www." + line.strip()):
         response = requests.get("http://www." + line.strip())
-        print("http://www." + line.strip(), response.status_code)
+        print("http://www." + line.strip(), " ", response.status_code)
     else:
         print(line.strip() + " is malformed.")
